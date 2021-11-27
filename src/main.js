b6e05642eadb06677fcbe52602e566d8baa9a183
KaterinTello <u.kate.tello@gmail.com>
2021-11-27 12:14:27 -0500

    rama en modo prueba en equipo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 // Este es el punto de entrada de tu aplicacion
 
+// import { async } from 'regenerator-runtime';
 import { myFunction } from './lib/index.js';
+import { homeView } from './lib/home.js';
 
 myFunction();
+homeView();
+
+const routes = {
+  '/': homeView,
+};
+
+console.log(routes);
+
+const router = async () => {
+  const header = null || document.getElementById('header');
+
+  header.innerHTML = await homeView();
+};
+
+window.addEventListener('load', router);
