b498e272a0ac05c0007f957b2dc9b345fb6f5849
KaterinTello <u.kate.tello@gmail.com>
2021-11-27 12:16:24 -0500

    se regresaron los cambios al inicio

--- src/main.js
+++ src/main.js
@@ -2,21 +2,5 @@
 
 // import { async } from 'regenerator-runtime';
 import { myFunction } from './lib/index.js';
-import { homeView } from './lib/home.js';
 
 myFunction();
-homeView();
-
-const routes = {
-  '/': homeView,
-};
-
-console.log(routes);
-
-const router = async () => {
-  const header = null || document.getElementById('header');
-
-  header.innerHTML = await homeView();
-};
-
-window.addEventListener('load', router);
