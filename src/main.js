b23c58f3b5b5887867f9b929c77320a29c03c94b
Rosamaria Rodriguez <rous9716@gmail.com>
2021-11-28 18:59:54 -0500

    Fix: Alert for creat user

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import { changeTmp } from './view-controller/router.js';
 
 myFunction();
 const init = () => {
-    changeTmp(window.location.hash);
-    window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+  changeTmp(window.location.hash);
+  window.addEventListener('hashchange', () => changeTmp(window.location.hash));
 };
 
 window.addEventListener('load', init);
-
-
