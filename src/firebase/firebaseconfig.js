2434842cbf53a54546929971b2fc4e590a22b2da
Rosamaria Rodriguez <rous9716@gmail.com>
2021-11-28 18:31:04 -0500

    Fix: Primer avance

---
+++ src/firebase/firebaseconfig.js
@@ -0,0 +1,19 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBMHBj3mvaU8cyKXAkEz2X62sk-rFf7Lo4",
+//   authDomain: "ecogram-81736.firebaseapp.com",
+//   projectId: "ecogram-81736",
+//   storageBucket: "ecogram-81736.appspot.com",
+//   messagingSenderId: "410575839464",
+//   appId: "1:410575839464:web:7dc1fe85b94bc93c9c295d",
+//   measurementId: "G-CS1JLZS72H"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
---
+++ src/views-template/home.js
@@ -0,0 +1,30 @@
+export const home = `
+<section class="home-container">
+  <div class="head-home">
+
+    <img src="/src/img/Logos/Logo1.png"/>
+    <h1>ECOGRAM</h1>
+    <img src="" class="img-home"/>
+
+  </div>
+  <div class= "btn-container-home">
+
+    <button-action class="secondary" id="btn-google">
+        <img src="" style="width: 20px; margin-right: 5px;" slot="image"/><span slot="title">Continuar con Google</span>
+    </button-action> 
+
+    <button-action class="primary" id="btn-login">
+        <span slot="title">Iniciar sesion</span>
+    </button-action>
+
+    <button-action class="secondary" id="btn-register">
+        <span slot="title">Registrarse</span>
+    </button-action>
+
+    <img src="" class="img-home1"/>  
+    <img src="" class="img-home2"/>  
+    <img src="" class="img-home3"/>  
+    <img src="" class="img-home4"/>  
+
+  </div>
+</section>`;
