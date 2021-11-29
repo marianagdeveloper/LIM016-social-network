eca7b53a08fa484103b8678164dadd7002ac9ad7
KaterinTello <u.kate.tello@gmail.com>
2021-11-28 19:14:06 -0500

    agregando cambios

---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+// eslint-disable-next-line import/no-unresolved
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js';
+// eslint-disable-next-line import/no-unresolved
+import { getFirestore, getDocs, collection } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js';
+
+const main = document.getElementById('main');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCk8ZsvxtMvGdSDjbG9qZwEtjRbW7ub9J4',
+  authDomain: 'ecogram-5151.firebaseapp.com',
+  projectId: 'ecogram-5151',
+  storageBucket: 'ecogram-5151.appspot.com',
+  messagingSenderId: '477155575466',
+  appId: '1:477155575466:web:3ad053e41029cfd7ebb2d3',
+  measurementId: 'G-0Y14YJN8Q0',
+};
+
+// Initialize Firebase app
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'usuarios');
+
+// get collection data
+
+getDocs(colRef)
+  .then((snapshot) => {
+    const usuarios = [];
+    snapshot.docs.forEach((doc) => {
+      usuarios.push({ ...doc.data(), id: doc.id });
+    });
+    // console.log(usuarios[0].title);
+    main.innerHTML = usuarios[0].title;
+  });
+  
+// .catch(err => {
+//   console.log(err.message)
+// });
