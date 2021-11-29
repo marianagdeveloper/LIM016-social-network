9244d55fa112fe89b002a09b1db5894417c09512
KaterinTello <u.kate.tello@gmail.com>
2021-11-29 11:21:46 -0500

    se hicieron cambios en espaciados

--- src/view-controller/router.js
+++ src/view-controller/router.js
@@ -1,12 +1,12 @@
-import { components } from "../pages/index.js";
 // import { createUser } from '../firebase/index.js';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js';
 import {
   getAuth,
   createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js";
+} from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
+import { components } from '../pages/index.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,13 +14,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCk8ZsvxtMvGdSDjbG9qZwEtjRbW7ub9J4",
-  authDomain: "ecogram-5151.firebaseapp.com",
-  projectId: "ecogram-5151",
-  storageBucket: "ecogram-5151.appspot.com",
-  messagingSenderId: "477155575466",
-  appId: "1:477155575466:web:a58327dc906a17f4ebb2d3",
-  measurementId: "G-ETL5GSYXTL",
+  apiKey: 'AIzaSyCk8ZsvxtMvGdSDjbG9qZwEtjRbW7ub9J4',
+  authDomain: 'ecogram-5151.firebaseapp.com',
+  projectId: 'ecogram-5151',
+  storageBucket: 'ecogram-5151.appspot.com',
+  messagingSenderId: '477155575466',
+  appId: '1:477155575466:web:a58327dc906a17f4ebb2d3',
+  measurementId: 'G-ETL5GSYXTL',
 };
 
 // Initialize Firebase
@@ -29,42 +29,42 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 export const changeTmp = (hash) => {
-  const id = hash.split("/")[1];
-  const sectionMain = document.getElementById("container");
-  sectionMain.innerHTML = "";
+  const id = hash.split('/')[1];
+  const sectionMain = document.getElementById('container');
+  sectionMain.innerHTML = '';
 
   switch (hash) {
-    case "":
-    case "#":
-    case "#/": {
+    case '':
+    case '#':
+    case '#/': {
       return sectionMain.appendChild(components.signin());
     }
-    case "#/signin":
-    case "#/signup": {
+    case '#/signin':
+    case '#/signup': {
       sectionMain.appendChild(components[id]());
-      document.getElementById("signup").addEventListener("click", function () {
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("pass").value;
+      document.getElementById('signup').addEventListener('click', () => {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('pass').value;
 
         createUserWithEmailAndPassword(auth, email, password)
           .then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            console.log("created");
-            alert("Created User")
+            console.log('created');
+            alert('Created User');
             // ...
           })
           .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             // ..
-            console.log("Notification:" + errorCode + errorMessage);
-            alert("Notification: " + errorMessage)
+            console.log(`Notification:${errorCode}${errorMessage}`);
+            alert(`Notification: ${errorMessage}`);
           });
       });
       return;
     }
-    case "#/home": {
+    case '#/home': {
       return sectionMain.appendChild(components[id]());
     }
     default:
