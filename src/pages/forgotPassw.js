004e7df8da6478f089a7e846eac32c20dfc9ff9b
KaterinTello <u.kate.tello@gmail.com>
2021-12-02 21:48:54 -0500

    add modulate pages signin,signup,forgotPass

--- src/pages/forgotPassw.js
+++ src/pages/forgotPassw.js
@@ -1,4 +1,55 @@
-export default () => {
+import {
+  auth, sendPasswordResetEmail, signInWithPopup, provider, GoogleAuthProvider,
+} from '../utils/firebaseconfig.js';
+
+export const handleForgotPassw = (e) => {
+  e.preventDefault();
+  const email = e.target.closest('form').querySelector('#email').value;
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      document
+        .getElementById('alertMessage')
+        .classList.replace('ocultAlertMessage', 'alertMessage');
+      // document.getElementById('alertMessage').classList.add('alertMessage');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      console.log('error en password', errorMessage, errorCode);
+      return errorMessage;
+    });
+};
+
+export const handleSigninGoogle = (e) => {
+  // document
+  //   .getElementById('btn-signin-google')
+  //   .addEventListener('click', (e) => {
+  e.preventDefault();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+      console.log(user.displayName + token);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      console.log(errorCode, errorMessage, email, credential);
+    });
+};
+
+const forgotPass = () => {
   const viewAccessories = `
     <div class="BoxContainerSignIn">
       <div class="BoxLogin">
@@ -44,5 +95,15 @@ export default () => {
   const divElemt = document.createElement('div');
   divElemt.classList.add('position');
   divElemt.innerHTML = viewAccessories;
+  // forgot password
+  divElemt
+    .querySelector('#btn-signin-signin')
+    .addEventListener('click', handleForgotPassw);
+
+  divElemt
+    .querySelector('#btn-signin-google')
+    .addEventListener('click', handleSigninGoogle);
   return divElemt;
 };
+
+export default forgotPass;
