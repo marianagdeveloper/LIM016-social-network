6c8c78a14a7162a9f8bd23ae23a784315d0cdbe7
Rosamaria Rodriguez <rous9716@gmail.com>
2021-12-05 01:01:30 -0500

    Style responsive nav toggle

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -4,14 +4,14 @@ const AboutUs = () => {
     // eslint-disable-next-line no-unused-expressions
     `
     <!-- ABOUT US PAGE -->
-      <section id='AboutUs' class='Box'>
-        <div class='LogoLargeDiv'>
-          <img class='LogoLarge' src='/src/img/Logos/LogoG7.png' alt='Ecogram'>
+      <section id="AboutUs" class="Box">
+        <div class="LogoLargeDiv">
+          <img class="LogoLarge" src="/src/img/Logos/LogoG7.png" alt="Ecogram">
         </div><br>
-        <div class='AboutUsText'>
+        <div class="AboutUsText">
           <h1>ECOGRAM</h1>
           <h3>EVERONMENTAL SOCIAL NETWORK</h3><br>
-          <p>It is a social network created for pro-environmental purposes. You will meet people from different countries with similar interests in caring for our only home 'Planet Earth'. If you don't take care of it, who will? </p>
+          <p>It is a social network created for pro-environmental purposes. You will meet people from different countries with similar interests in caring for our only home "Planet Earth". If you don"t take care of it, who will? </p>
           <br>
           <h1>WHAT WILL YOU FIND IN ECOGRAM?</h1>
           <h3>On this platform you can find the following sections:</h3><br>
@@ -33,42 +33,42 @@ const AboutUs = () => {
             </li>
           </ul>
         </div>
-        <div class='FounderImg'>
+        <div class="FounderImg">
           <h1>FOUNDERS:</h1>
-          <img src='/src/img/Logos/KMR4.png' alt='Founders'>
+          <img src="/src/img/Logos/KMR4.png" alt="Founders">
         </div><br>
 
         <div class="Founders-Box">
           <div class="Founder-Box-Box">
-            <a href="https://github.com/KaterinT">
+            <a target="_blank" href="https://github.com/KaterinT">
               <img src="/src/img/RedesSociales/github-120.png" class="redSocial" alt="Github">
             </a>
-            <a href="mailto:tellourakaterin@gmail.com">
+            <a target="_blank" href="mailto:tellourakaterin@gmail.com">
               <img src="/src/img/RedesSociales/gmail-120.png" class="redSocial" alt="Gmail">
             </a>
-            <a href=""><img src="/src/img/RedesSociales/linkedin-120.png" class="redSocial" alt="LinkenIn"></a>
+            <a target="_blank" href=""><img src="/src/img/RedesSociales/linkedin-120.png" class="redSocial" alt="LinkenIn"></a>
             <p>Katerin Tello</p>
           </div>
           <div class="Founder-Box-Box">
-            <a href="https://github.com/marianagdeveloper">
+            <a target="_blank" href="https://github.com/marianagdeveloper">
               <img src="/src/img/RedesSociales/github-120.png" class="redSocial" alt="Github">
             </a>
-            <a href="mailto:">
+            <a target="_blank" href="mailto:">
               <img src="/src/img/RedesSociales/gmail-120.png" class="redSocial" alt="Gmail">
             </a>
-            <a href="">
+            <a target="_blank" href="">
               <img src="/src/img/RedesSociales/linkedin-120.png" class="redSocial" alt="LinkenIn">
             </a>
             <p>Mariana Guanda</p>
           </div>
           <div class="Founder-Box-Box">
-            <a href="https://github.com/RJRCH123">
+            <a target="_blank" href="https://github.com/RJRCH123">
               <img src="/src/img/RedesSociales/github-120.png" class="redSocial" alt="Github">
             </a>
-            <a href="mailto:rous9716@gmail.com">
+            <a target="_blank" href="mailto:rous9716@gmail.com">
               <img src="/src/img/RedesSociales/gmail-120.png" class="redSocial" alt="Gmail">
             </a>
-            <a href="https://www.linkedin.com/in/rosamaria-rodriguez-6904a1191/">
+            <a target="_blank" href="https://www.linkedin.com/in/rosamaria-rodriguez-6904a1191/">
               <img src="/src/img/RedesSociales/linkedin-120.png" class="redSocial" alt="LinkenIn">
             </a>
             <p>Rosamaria Rodriguez</p>
