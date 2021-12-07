3640656782ce0359027e43c7afdd9891f238e376
KaterinTello <u.kate.tello@gmail.com>
2021-12-06 22:15:04 -0500

    added user search view

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,170 @@
 const Search = () => {
   const viewSearch = `      
-  <section id="Search" class="Box">
-  </section>`;
+    <main class="main">
+    <div class="container">
+      <div class="caja1">
+        <div class="input">
+          <input type="text" id="fname" name="firstname" placeholder="Your name..">
+        </div>
+        <div class="filtros">
+          <div class="filtro1">
+            <input list="browsers" name="myBrowser" />
+              <datalist id="browsers">
+                <option value="Chrome">
+                <option value="Firefox">
+                <option value="Internet Explorer">
+                <option value="Opera">
+                <option value="Safari">
+                <option value="Microsoft Edge">
+              </datalist>
+          </div>
+          <div class="filtro2">
+            <input list="browsers" name="myBrowser" />
+            <datalist id="browsers">
+              <option value="Chrome">
+              <option value="Firefox">
+              <option value="Internet Explorer">
+              <option value="Opera">
+              <option value="Safari">
+              <option value="Microsoft Edge">
+            </datalist>
+          </div>
+        </div>
+      </div>
+      <div class="caja2">
+        <div class="search">
+          <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div>  
+        </div>
+        <div class="search">
+          <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> 
+        </div>
+        <div class="search">
+          <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> 
+        </div>
+        <div class="search">
+          <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> 
+        </div>
+        <div class="search">
+          <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> 
+        </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        <div class="search">   <div class="perfil"><img class="imgPerfil" src="/src/img/Avatares/Animals/AvatarA1.png" alt=""></div>
+          <div class="caracteres">
+            <div class="nombre">JUANITO ARISTISABAL</div>
+            <div class="pais">Perú</div>
+            <div class="intereses">
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AguaCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+              <div class="imgCaracteres"><img src="/src/img/Intereses/InteresesCN/AnimalCN.png" alt=""></div>
+            </div>
+          </div> </div>
+        </div>
+      </div>
+    </main>`;
 
   const divElemt = document.createElement('div');
   divElemt.innerHTML = viewSearch;
