c64b21ed34b4a8282e7643860247e1022c33af61
Rosamaria Rodriguez <rous9716@gmail.com>
2021-12-16 16:36:16 -0500

    Resolve conflicts of Eslint

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-sequences */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-plusplus */
-
 import { publicationComponent } from './publication.js';
 
 import {
@@ -16,11 +11,11 @@ import {
   deleteDoc,
 } from '../utils/firebaseconfig.js';
 
-//modal
-let cerrar = document.getElementById('close');
-let modalC = document.getElementById('modal-container');
-let btnModalConfirmDelete = document.getElementById('btn-modal-yes');
-let btnModalCancel = document.getElementById('btn-modal-no');
+// modal
+const cerrar = document.getElementById('close');
+const modalC = document.getElementById('modal-container');
+const btnModalConfirmDelete = document.getElementById('btn-modal-yes');
+const btnModalCancel = document.getElementById('btn-modal-no');
 
 // Obtener un usuario de Firebase
 async function readUser(uid) {
@@ -243,7 +238,7 @@ const Home = () => {
     return uidSS;
   };
   readUser(uid())
-    .then((value) => { infoUser(value), reedPublications(value); })
+    .then((value) => {infoUser(value), reedPublications(value)})
     .catch((error) => console.log(error));
 
   // actualizacion tiempo real de publications
@@ -262,18 +257,21 @@ const Home = () => {
       const nameUser = userOfPublication.data().name;
       const publicationText = documentFirebase.data().publication;
 
-      //Only Delete or Edit Post for UserCurrent
-      let authorPublication = userOfPublication.data().uid;
-      let userCurrent = sessionStorage.getItem('key');
-      let myPost = authorPublication === userCurrent;
-      let visibilityImg = "hidden";
+      // Only Delete or Edit Post for UserCurrent
+      const authorPublication = userOfPublication.data().uid;
+      const userCurrent = sessionStorage.getItem('key');
+      const myPost = authorPublication === userCurrent;
+      let visibilityImg = 'hidden';
       if (myPost) {
-        visibilityImg = "visible";
+        visibilityImg = 'visible';
       }
 
-      //add componet publication___________________________________________________________-
-      divPublicado.prepend(publicationComponent(nameUser, myPost, visibilityImg, idPublication, publicationText));
-
+      // add componet publication
+      divPublicado.prepend(publicationComponent(nameUser,
+        myPost,
+        visibilityImg,
+        idPublication,
+        publicationText));
 
       // delete publication
       const publication = divPublicado.querySelectorAll('img[data-ref]');
@@ -282,43 +280,43 @@ const Home = () => {
           e.preventDefault();
           const idPublicationRef = element.dataset.ref;
 
-          //INIT - Modal for Vericate Delete Publication
+          // INIT - Modal for Vericate Delete Publication
           let stateModal = false;
 
-          //view modal
-          modalC.style.opacity = "1";
-          modalC.style.visibility = "visible";
+          // view modal
+          modalC.style.opacity = '1';
+          modalC.style.visibility = 'visible';
 
-          //close modal
-          cerrar.addEventListener("click", function () {
-            modalC.style.opacity = "0";
-            modalC.style.visibility = "hidden";
+          // close modal
+          cerrar.addEventListener('click', () => {
+            modalC.style.opacity = '0';
+            modalC.style.visibility = 'hidden';
             return stateModal;
           });
 
-          //cancel modal
-          btnModalCancel.addEventListener("click", function () {
-            modalC.style.opacity = "0";
-            modalC.style.visibility = "hidden";
-            return stateModal
+          // cancel modal
+          btnModalCancel.addEventListener('click', () => {
+            modalC.style.opacity = '0';
+            modalC.style.visibility = 'hidden';
+            return stateModal;
           });
 
-          //confirm delete - YES
-          btnModalConfirmDelete.addEventListener("click", function () {
-            modalC.style.opacity = "0";
-            modalC.style.visibility = "hidden";
+          // confirm delete - YES
+          btnModalConfirmDelete.addEventListener('click', () => {
+            modalC.style.opacity = '0';
+            modalC.style.visibility = 'hidden';
             stateModal = true;
 
             // Delete publication for Firebase
             deletePublication(idPublicationRef, divPublicado);
 
-            //Delete box of publications
+            // Delete box of publications
             const elementDelete = element.parentNode.parentNode.parentNode;
             elementDelete.remove();
 
-            return stateModal
+            return stateModal;
           });
-          //END - Modal for Vericate Delete Publication
+          // END - Modal for Vericate Delete Publication
         });
       });
     } else {
