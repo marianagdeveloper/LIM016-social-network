e5be2e97807f6b49a2f48ed62d39e45e45e10486
marianagdeveloper <mariana.guanda@gmail.com>
2021-12-25 17:56:57 -0500

    add filter interests

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,8 +1,16 @@
-import { getDocs, collection, db } from '../utils/firebaseconfig.js';
+import {
+  getDocs,
+  collection,
+  db,
+  query,
+  where,
+} from "../utils/firebaseconfig.js";
+import { countries } from "../utils/countries.js";
+import { interests } from "../utils/interests.js";
 
 //Function - Get Users data from Firestore
 async function usersInFirestore() {
-  const usersData = await getDocs(collection(db, 'users'));
+  const usersData = await getDocs(collection(db, "users"));
   return usersData;
 }
 
@@ -16,27 +24,13 @@ const Search = () => {
         </div>
         <div class='filtros'>
           <div class='filtro1'>
-              <select name='select'>
-                <option value='Country'>Country</opcion>
-                <option value='Peru'>Perú</opcion>
-                <option value='Mexico'>México</opcion>
-                <option value='Chile'>Chile</opcion>
-                <option value='Venezuela'>Venezuela</opcion>
-                <option value='Argentina'>Argentina</opcion>
-                <option value='Nicaragua'>Nicaragua</opcion>
+              <select class='selectCountry' name='select'>
+                <option value='Country'>Country</option>
               </select>
           </div>
-              
-
           <div class='filtro2'>
-            <select name='select'>
+            <select  class='selectInterest' name='select'>
               <option value='Interest'>Interest</opcion>
-              <option value='Energia'>Energía</opcion>
-              <option value='Agua'>Agua</opcion>
-              <option value='Reciclaje'>Reciclaje</opcion>
-              <option value='Siembra'>Siembra</opcion>
-              <option value='Argentina'>Argentina</opcion>
-              <option value='Nicaragua'>Nicaragua</opcion>
             </select>
           </div>
         </div>
@@ -44,34 +38,76 @@ const Search = () => {
       <div class='caja2'></div>
       </div>
 `;
-  const divElemt = document.createElement('div');
+  const divElemt = document.createElement("div");
   divElemt.innerHTML = viewSearch;
 
-  let dataUsers;
+  //Clear 
+  const clearCards =() => {
+    while (divCardUser.firstChild) {
+      divCardUser.firstChild.remove();
+    }
+  }
+
+  //Divs querySelector
+  const divCardUser = divElemt.querySelector(".caja2");
+  const divSelectCountry = divElemt.querySelector(".selectCountry");
+  const divSelectInterest = divElemt.querySelector(".selectInterest");
+
+  // Filter
+  async function filterUsers(key, divElem) {
+    clearCards();
+    let filterInfo = divElem;
+    let q =  query(collection(db, "users"));
+    if (key == 'country'){
+      q = query(collection(db, "users"), where("country", "==", filterInfo.value));
+    }
+    if (key == 'interests'){
+      q = query(collection(db, "users"), where("interests", "array-contains", filterInfo.value));
+    }
+    const querySnapshot = await getDocs(q);
+    printDataUsers(querySnapshot)
+  }
+
+  //Show countries
+  // eslint-disable-next-line no-restricted-syntax
+  for (const prop in countries) {
+    divSelectCountry.innerHTML += `<option value='${prop}:${countries[prop]}'>${countries[prop]}</option>`;
+  }
+  divSelectCountry.addEventListener("change", () => {filterUsers('country', divSelectCountry)});
+
+  // Show Select Interest
+  // eslint-disable-next-line no-restricted-syntax
+  for (const prop in interests) {
+    divSelectInterest.innerHTML += `<option value='${interests[prop]}'>${prop}</option>`;
+  }
+  divSelectInterest.addEventListener("change", () => {
+    // console.log('divSelectInterest:', divSelectInterest.value);
+    filterUsers('interests', divSelectInterest);
+  });
 
   //Print Users
   function printDataUsers(data) {
-    dataUsers = data;
+    // console.log('data: ', data);
+    let dataUsers = data;
+    
     dataUsers.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data());
+      // console.log(doc.id, " => ", doc.data());
       //Print One User
       let user, photo, fullname, country, interests;
       user = doc.data();
-      photo = user.photo; 
-      fullname = user.name; 
-      country = user.country.split(':')[1]; 
+      photo = user.photo;
+      fullname = user.name;
+      country = user.country.split(":")[1];
       interests = user.interests;
       printUser(photo, fullname, country, interests);
     });
-    return
+    return;
   }
 
   //Print One card user
-  function printUser(photo, fullname, country, interests){
-    const divCardUser = divElemt.querySelector('.caja2')
-  divCardUser.innerHTML += `
-  <div class='search'>
+  function printUser(photo, fullname, country, interests) {
+    divCardUser.innerHTML += `
+        <div class='search'>
           <div class='perfil'><img class='imgPerfil' src='${photo}' alt=''></div>
           <div class='caracteres'>
             <div class='nombre'>${fullname}</div>
@@ -84,8 +120,8 @@ const Search = () => {
           </div>  
         </div>
   `;
-  return
-  };
+    return;
+  }
 
   //Users in Firestore and print
   const infoUsers = () => {
@@ -96,7 +132,7 @@ const Search = () => {
         printDataUsers(data);
       })
       .catch((err) => {
-        console.log('err: ', err);
+        console.log("err: ", err);
       });
   };
 
