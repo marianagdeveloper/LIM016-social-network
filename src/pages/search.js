82255d5a1ede1e0e21401af910c71d6d0a426171
marianagdeveloper <mariana.guanda@gmail.com>
2021-12-28 20:41:51 -0500

    fix search bio en card

--- src/pages/search.js
+++ src/pages/search.js
@@ -129,7 +129,11 @@ const Search = () => {
   function printUser(photo, fullname, country, interests, bio) {
     divCardUser.innerHTML += `
         <div class='search'>
-          <div class='perfil'><img class='imgPerfil' src='${photo}' alt=''>           <button id="btnSeeUserPost" class="btnSeeUserPost" data-userPost:  ><a href="#/home">See Posts</a></button></div>
+          <div class='perfil'>
+          <img class='imgPerfil' src='${photo}' alt=''>
+          <button id="btnSeeUserPost" class="btnSeeUserPost">
+            <a href="#/home">See Posts</a>
+          </button></div>
           <div class='caracteres'>
             <div class='nombre'>${fullname}</div>
             <div class='pais'>${country}</div>
@@ -139,7 +143,7 @@ const Search = () => {
               <div class='imgCaracteres'><img src='${interests[2]}' alt=''></div>
             </div>
             <div class='flexBtn'>
-              <p id="txtBioPost" class="txtBioPost" data-userPost:  >Bio : ${bio}</p>
+              <p id="txtBioPost" class="txtBioPost">${bio}</p>
             </div>
           </div>  
         </div>
